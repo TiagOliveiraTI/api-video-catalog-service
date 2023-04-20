27f04f14bb10b6121f9cfb3410038618b519a19e
Tiago Oliveira <dev.tiago.oliveira@gmail.com>
2023-04-20 14:59:01 -0300

    test: create update method

--- tests/Unit/Core/Domain/Entity/CategoryUnitTest.php
+++ tests/Unit/Core/Domain/Entity/CategoryUnitTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Core\Domain\Dto\CategoryInputDto;
 use Core\Domain\Entity\Category;
 
 it('should test the class attributes', function() {
@@ -40,4 +41,33 @@ it('should be disabled', function () {
     
     expect($sut->isActive)->toBeFalsy();
 
+});
+
+it('should update the category correctly', function () {
+    // Arrange
+    $uuid = 'uuid.value';
+
+    $inputDto = new CategoryInputDto(
+        name: 'example_category_name',
+        description: 'example_category_description',
+        isActive: true,
+    );
+
+    $sut = new Category(
+        id: $uuid,
+        name: $inputDto->name,
+        description: $inputDto->description,
+        isActive: $inputDto->isActive,
+    );
+
+    // Act
+    $sut->update(
+        name: 'new_category_name',
+        description: 'new_category_description',
+    );
+
+    // Assert
+    expect($sut->name)->toBe('new_category_name');
+    expect($sut->description)->toBe('new_category_description');
+    expect($sut->isActive)->toBeTrue();
 });
