50b71c521f58b8b405addef44cf425f50818221d
Tiago Oliveira <contato@otao.dev.br>
2023-06-19 00:19:46 +0000

    test: add validations to method  stringMaxLength

--- tests/Unit/Core/Domain/Entity/Validation/DomainValidationTest.php
+++ tests/Unit/Core/Domain/Entity/Validation/DomainValidationTest.php
@@ -25,4 +25,52 @@ class DomainValidationTest extends TestCase
 
         DomainValidation::notNull('', 'any message.');
     }
+
+    public function testStringMaxLengthThrowsExceptionForMaxLength()
+    {
+        $string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+        $maxValue = 20;
+        $exceptionMessage = 'Custom exception message';
+
+        $this->expectException(EntityValidationException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        DomainValidation::stringMaxLength($string, $maxValue, $exceptionMessage);
+    }
+
+    public function testStringMaxLengthThrowsExceptionForEqualMaxLength()
+    {
+        $string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+        $maxValue = strlen($string);
+        $exceptionMessage = null;
+
+        $this->expectException(EntityValidationException::class);
+        $this->expectExceptionMessage("the max value is {$maxValue} characters.");
+
+        DomainValidation::stringMaxLength($string, $maxValue, $exceptionMessage);
+    }
+
+    public function testStringMaxLengthDoesNotThrowExceptionForLessThanMaxLength()
+    {
+        $string = 'Short string';
+        $maxValue = 255;
+        $exceptionMessage = null;
+
+        $this->expectNotToPerformAssertions();
+
+        DomainValidation::stringMaxLength($string, $maxValue, $exceptionMessage);
+    }
+
+    public function testStringMaxLengthThrowsExceptionWithCustomExceptionMessage()
+    {
+        $string = 'Too long string';
+        $maxValue = 10;
+        $exceptionMessage = 'Custom exception message';
+
+        $this->expectException(EntityValidationException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        DomainValidation::stringMaxLength($string, $maxValue, $exceptionMessage);
+    }
+
 }
